eb187f06928fe299398eb4d9493b0d81fe127d50
TejasPhalke <157502608+TejasPhalke228@users.noreply.github.com>
2025-09-18 12:06:53 +0530

    app.py

--- Emotion_music_app/app.py
+++ Emotion_music_app/app.py
@@ -16,27 +16,26 @@ if not os.path.exists(app.config['UPLOAD_FOLDER']):
 # Emotion to Bollywood songs mapping
 emotion_playlist = {
     'happy': [
-        "https://drive.google.com/uc?export=download&id=13bfaZXgsGNPPkD_pA-Rkm8FXTNO3bQeD",
-        "https://drive.google.com/uc?export=download&id=1JdXgcvcfiqftvr5-CLcGGXvRx6WOKMjq",
-	"https://drive.google.com/uc?export=download&id=1R6BWqjS2x22C3bziEuhhBLfhvbSw8S5B",
-	"https://drive.google.com/uc?export=download&id=1EvDoLKpCbcwyK4ArKaHr4Z8U4M7S0X70"
+        {"title": "pehla nasha", "url":"https://drive.google.com/uc?export=download&id=13bfaZXgsGNPPkD_pA-Rkm8FXTNO3bQeD",
+        {"title": "tujhe dekha", "url":"https://drive.google.com/uc?export=download&id=1JdXgcvcfiqftvr5-CLcGGXvRx6WOKMjq"},
+		{"title": "badtameez dil", "url":"https://drive.google.com/uc?export=download&id=1R6BWqjS2x22C3bziEuhhBLfhvbSw8S5B"},
+		{"title": "ilahi", "url":"https://drive.google.com/uc?export=download&id=1EvDoLKpCbcwyK4ArKaHr4Z8U4M7S0X70"}
     ],
     'sad': [
-        "https://drive.google.com/uc?export=download&id=1xQs78QDm6Keh-3c5R02gX6JJUxBxP0E6",
-        "https://drive.google.com/uc?export=download&id=1VGPTp2-xN1lRUF36P3yfR5z9BbvTnS4r",
-	"https://drive.google.com/uc?export=download&id=1uAjVLL8pSNb5FTc9sDQpMPxH-5VE1jDn"
+        {"title": "channa mereya", "url":"https://drive.google.com/uc?export=download&id=1xQs78QDm6Keh-3c5R02gX6JJUxBxP0E6"},
+        {"title": "tum hi ho", "url":"https://drive.google.com/uc?export=download&id=1VGPTp2-xN1lRUF36P3yfR5z9BbvTnS4r"},
+	{"title": "dooriyan", "url":"https://drive.google.com/uc?export=download&id=1uAjVLL8pSNb5FTc9sDQpMPxH-5VE1jDn"}
     ],
     'angry': [
-        "https://drive.google.com/uc?export=download&id=1reSryO2rgIjWGdZ2PgZbno5nmmcywWpH",
-        "https://drive.google.com/uc?export=download&id=116l9rKuM1YHYtPnqFW-vk76Ms7E7dq-R"
+       {"title": "saada haq", "url":"https://drive.google.com/uc?export=download&id=1reSryO2rgIjWGdZ2PgZbno5nmmcywWpH"},
+        {"title": "bulleya", "url":"https://drive.google.com/uc?export=download&id=116l9rKuM1YHYtPnqFW-vk76Ms7E7dq-R"}
     ],
     'surprise': [
-        "https://drive.google.com/uc?export=download&id=1dIEjD7HH_FpndU9Px1oGQIiwNr_Re-4Q"
+        {"title": "ek ladki", "url":"https://drive.google.com/uc?export=download&id=1dIEjD7HH_FpndU9Px1oGQIiwNr_Re-4Q"}
     ],
     'neutral': [
-        "music/ae_mere_humsafar.mp3",
-	"https://drive.google.com/uc?export=download&id=1OBw5YVlxMsAoNKNT9DEj0dkZyOb2lLw7",
-	"https://drive.google.com/uc?export=download&id=1C0z_LLbvoGR1qKM8a94fUZsHijR6NBTp"
+	{"title": "kabira", "url":"https://drive.google.com/uc?export=download&id=1OBw5YVlxMsAoNKNT9DEj0dkZyOb2lLw7"},
+	{"title": "tera hone", "url":"https://drive.google.com/uc?export=download&id=1C0z_LLbvoGR1qKM8a94fUZsHijR6NBTp"}
     ]
 }
 
@@ -73,4 +72,5 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
