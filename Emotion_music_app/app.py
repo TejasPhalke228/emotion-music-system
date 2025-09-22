587a430d33896797c43e937b2446eae06634350c
TejasPhalke <157502608+TejasPhalke228@users.noreply.github.com>
2025-09-22 11:48:11 +0530

    app.py

--- Emotion_music_app/app.py
+++ Emotion_music_app/app.py
@@ -13,29 +13,29 @@ app.config['UPLOAD_FOLDER'] = 'uploads'
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
-# Emotion to Bollywood songs mapping
+# Emotion to Bollywood songs mapping (preview links)
 emotion_playlist = {
     'happy': [
-        {"title": "pehla nasha", "url":"https://drive.google.com/uc?export=download&id=13bfaZXgsGNPPkD_pA-Rkm8FXTNO3bQeD"},
-        {"title": "tujhe dekha", "url":"https://drive.google.com/uc?export=download&id=1JdXgcvcfiqftvr5-CLcGGXvRx6WOKMjq"},
-		{"title": "badtameez dil", "url":"https://drive.google.com/uc?export=download&id=1R6BWqjS2x22C3bziEuhhBLfhvbSw8S5B"},
-		{"title": "ilahi", "url":"https://drive.google.com/uc?export=download&id=1EvDoLKpCbcwyK4ArKaHr4Z8U4M7S0X70"}
+        {"title": "pehla nasha", "url": "https://drive.google.com/file/d/13bfaZXgsGNPPkD_pA-Rkm8FXTNO3bQeD/preview"},
+        {"title": "tujhe dekha", "url": "https://drive.google.com/file/d/1JdXgcvcfiqftvr5-CLcGGXvRx6WOKMjq/preview"},
+        {"title": "badtameez dil", "url": "https://drive.google.com/file/d/1R6BWqjS2x22C3bziEuhhBLfhvbSw8S5B/preview"},
+        {"title": "ilahi", "url": "https://drive.google.com/file/d/1EvDoLKpCbcwyK4ArKaHr4Z8U4M7S0X70/preview"}
     ],
     'sad': [
-        {"title": "channa mereya", "url":"https://drive.google.com/uc?export=download&id=1xQs78QDm6Keh-3c5R02gX6JJUxBxP0E6"},
-        {"title": "tum hi ho", "url":"https://drive.google.com/uc?export=download&id=1VGPTp2-xN1lRUF36P3yfR5z9BbvTnS4r"},
-	{"title": "dooriyan", "url":"https://drive.google.com/uc?export=download&id=1uAjVLL8pSNb5FTc9sDQpMPxH-5VE1jDn"}
+        {"title": "channa mereya", "url": "https://drive.google.com/file/d/1xQs78QDm6Keh-3c5R02gX6JJUxBxP0E6/preview"},
+        {"title": "tum hi ho", "url": "https://drive.google.com/file/d/1VGPTp2-xN1lRUF36P3yfR5z9BbvTnS4r/preview"},
+        {"title": "dooriyan", "url": "https://drive.google.com/file/d/1uAjVLL8pSNb5FTc9sDQpMPxH-5VE1jDn/preview"}
     ],
     'angry': [
-       {"title": "saada haq", "url":"https://drive.google.com/uc?export=download&id=1reSryO2rgIjWGdZ2PgZbno5nmmcywWpH"},
-        {"title": "bulleya", "url":"https://drive.google.com/uc?export=download&id=116l9rKuM1YHYtPnqFW-vk76Ms7E7dq-R"}
+        {"title": "saada haq", "url": "https://drive.google.com/file/d/1reSryO2rgIjWGdZ2PgZbno5nmmcywWpH/preview"},
+        {"title": "bulleya", "url": "https://drive.google.com/file/d/116l9rKuM1YHYtPnqFW-vk76Ms7E7dq-R/preview"}
     ],
     'surprise': [
-        {"title": "ek ladki", "url":"https://drive.google.com/uc?export=download&id=1dIEjD7HH_FpndU9Px1oGQIiwNr_Re-4Q"}
+        {"title": "ek ladki", "url": "https://drive.google.com/file/d/1dIEjD7HH_FpndU9Px1oGQIiwNr_Re-4Q/preview"}
     ],
     'neutral': [
-	{"title": "kabira", "url":"https://drive.google.com/uc?export=download&id=1OBw5YVlxMsAoNKNT9DEj0dkZyOb2lLw7"},
-	{"title": "tera hone", "url":"https://drive.google.com/uc?export=download&id=1C0z_LLbvoGR1qKM8a94fUZsHijR6NBTp"}
+        {"title": "kabira", "url": "https://drive.google.com/file/d/1OBw5YVlxMsAoNKNT9DEj0dkZyOb2lLw7/preview"},
+        {"title": "tera hone", "url": "https://drive.google.com/file/d/1C0z_LLbvoGR1qKM8a94fUZsHijR6NBTp/preview"}
     ]
 }
 
@@ -60,18 +60,13 @@ def index():
         except:
             emotion = 'neutral'
 
-        # Map emotions not in our simplified list to 'neutral'
         if emotion not in emotion_playlist:
             emotion = 'neutral'
 
         playlist = emotion_playlist.get(emotion, emotion_playlist['neutral'])
-
         os.remove(filepath)
 
     return render_template('index.html', emotion=emotion, playlist=playlist)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
